080c8724df9ffc3741db6276d9c894a44fd6bdaf
chris g <griggsca91@gmail.com>
2017-08-12 21:01:56 +0000

    Because I'm an idiot

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public/javascript');
+const APP_DIR = path.resolve(__dirname, 'public/javascript');
+
+let config = {
+    entry: APP_DIR + '/app.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'app.min.js'
+    },
+    module: {
+        loaders : [
+            {
+                test: /\.jsx?/,
+                include : APP_DIR,
+                loader : 'babel-loader'
+            }
+        ]
+    }
+};
+
+module.exports = config;
