1047d181ac30293b689d634facad78d3f433d0e9
Spencer Snyder <sasnyde2@gmail.com>
2017-08-11 22:02:04 -0400

    initial schema for network and station

---
+++ models/network.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from 'mongoose';
+
+const NetworkSchema = new Schema({
+  company: String,
+  href: String,
+  id: String,
+  location: {
+    city: String,
+    country: String,
+    latitude: {
+      type: Number,
+      isRequired: true,
+    },
+    longitude: {
+      type: Number,
+      isRequired: true,
+    },
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const Network = mongoose.Model('Network', NetworkSchema);
+
+export default Network;
---
+++ models/station.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from 'mongoose';
+
+const StationSchema = new Schema({
+  empty_slots: {
+    default: 0,
+    type: Number,
+    isRequired: true,
+  },
+  free_bikes: Number,
+  latitude: {
+    type: Number,
+    isRequired: true,
+  },
+  longitude: {
+    type: Number,
+    isRequired: true,
+  },
+  name: String,
+  timestamp: Date,
+});
+
+const Station = mongoose.model('Station', StationSchema);
+
+export default Station;
