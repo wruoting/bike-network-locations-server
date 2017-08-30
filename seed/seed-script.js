5d4d73035e689edfcec9e26e209ff43d199be5ed
ruotingw <wruoting@gmail.com>
2017-08-29 22:24:21 -0400

    ES6

---
+++ seed/seed-script.js
@@ -0,0 +1,19 @@
+// require('babel-core/register');
+// require('babel-polyfill');
+// require('../models/network.js')
+//
+// var mongoose = require('mongoose');
+// var read = require('read-file');
+//var network = require('../models/network.js')
+
+import mongoose, { Schema } from 'mongoose';
+import read from 'read-file';
+import Network from '../models/network.js'
+
+// var buffer = read.sync('./seed/seed.txt');
+
+// var uploadJSON = new NetworkSchema(buffer);
+
+
+
+console.log(uploadJSON);
